a6a986f19cfff34ad1e87215cfc05fc2292643b8
kishan upadhayay <kishan.upadhayay@mindinventory.com>
2020-10-12 18:25:25 +0530

    Responsive

--- src/app/main/logout/logout.component.ts
+++ src/app/main/logout/logout.component.ts
@@ -15,7 +15,6 @@ export class LogoutComponent implements OnInit {
 
   ngOnInit(): void {
     this.helperService.currentMessage.subscribe(message => this.message = message);
-    console.log('mess', this.message);
     if (this.message === '') {
       this.router.navigate(['/home']);
     }
